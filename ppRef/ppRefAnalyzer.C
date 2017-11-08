e583e28cbd200f299d6b5159e031fa06340070ee
Austin Baty <abaty@submit.mit.edu>
2017-11-08 18:55:48 -0500

    update to pp ref plot

--- ppRef/ppRefAnalyzer.C
+++ ppRef/ppRefAnalyzer.C
@@ -30,6 +30,14 @@
 
 void ppRefAnalyzer(){
   TH1::SetDefaultSumw2();
+  gStyle->SetOptStat(0);
+  gStyle->SetErrorX(0);
+  gStyle->SetLegendBorderSize(0);
+  gStyle->SetStatBorderSize(0);
+  gStyle->SetTitleBorderSize(0);
+  gStyle->SetPadTickY(1);
+  gStyle->SetPadTickX(1);
+
   Settings s;
 
 
@@ -41,18 +49,38 @@ void ppRefAnalyzer(){
 
   TH1D * pp5 = new TH1D("pp5","pp5",s.ntrkBins,s.xtrkbins);
   TH1D * pp5Syst = new TH1D("pp5Syst","pp5Syst",s.ntrkBins,s.xtrkbins);
+  TH1D * pp5relSyst = new TH1D("pp5relSyst","pp5relSyst",s.ntrkBins,s.xtrkbins);
+  TH1D * pp5relSyst_plus1 = new TH1D("pp5relSyst_plus1","pp5relSyst_plus1",s.ntrkBins,s.xtrkbins);
+  TH1D * pp5relSyst_minus1 = new TH1D("pp5relSyst_minus1","pp5relSyst_minus1",s.ntrkBins,s.xtrkbins);
+
+  TH1D * pythia8_5 = new TH1D("pythia8_5","pythia8_5",s.ntrkBins,s.xtrkbins);
+  TH1D * EPOS5 = new TH1D("EPOS_5","EPOS_5",s.ntrkBins,s.xtrkbins);
+  TH1D * pythia8_5rat;
+  TH1D * EPOS5rat;
+
   for(int i = 1; i<pp5->GetSize()-1; i++){
     pp5->SetBinContent(i,ppSpec->GetBinContent(i));
-    pp5->SetBinError(i,ppSpec_stat->GetBinContent(i));
-    pp5Syst->SetBinContent(i,TMath::Power(TMath::Power(ppSpec_syst->GetBinContent(i),2)+TMath::Power(ppSpec_lumi->GetBinContent(i),2),0.5));
+    pp5Syst->SetBinContent(i,TMath::Power(TMath::Power(ppSpec_syst->GetBinContent(i),2)+TMath::Power(ppSpec_lumi->GetBinContent(i),2)+TMath::Power(ppSpec_stat->GetBinContent(i),2),0.5));
+    pp5relSyst->SetBinContent(i,TMath::Power(TMath::Power(ppSpec_syst->GetBinContent(i),2)+TMath::Power(ppSpec_lumi->GetBinContent(i),2)+TMath::Power(ppSpec_stat->GetBinContent(i),2),0.5)/pp5->GetBinContent(i));
+    pp5relSyst_plus1->SetBinContent(i,1.+TMath::Power(TMath::Power(ppSpec_syst->GetBinContent(i),2)+TMath::Power(ppSpec_lumi->GetBinContent(i),2)+TMath::Power(ppSpec_stat->GetBinContent(i),2),0.5)/pp5->GetBinContent(i));
+    pp5relSyst_minus1->SetBinContent(i,1.-TMath::Power(TMath::Power(ppSpec_syst->GetBinContent(i),2)+TMath::Power(ppSpec_lumi->GetBinContent(i),2)+TMath::Power(ppSpec_stat->GetBinContent(i),2),0.5)/pp5->GetBinContent(i));
+  
+    pythia8_5->SetBinContent(i,70*1.3*pp5->GetBinContent(i));//FIXME
+    pythia8_5->SetBinError(i,70*0.05*pp5->GetBinContent(i));//FIXME
+    EPOS5->SetBinContent(i,70*0.8*pp5->GetBinContent(i));//FIXME
+    EPOS5->SetBinError(i,70*0.05*pp5->GetBinContent(i));//FIXME
   }
-  pp5->Print("All");
-  pp5Syst->Print("All");
 
+  pp5->Scale(70);
+  pp5Syst->Scale(70);
+  pythia8_5rat = (TH1D*)pythia8_5->Clone("pythia8_5rat");
+  pythia8_5rat->Divide(pp5);
+  EPOS5rat = (TH1D*)EPOS5->Clone("EPOS5rat");
+  EPOS5rat->Divide(pp5);
 
-  /*
   TCanvas * canv2 = new TCanvas("canv2","canv2",700,800);
   canv2->SetBorderSize(0);
+
   TPad * pad1 = new TPad("pad1","pad1",0.0,0.3,1.0,1.0,0);
   TPad * pad2 = new TPad("pad2","pad2",0.0,0.0,1.0,0.3,0);
   canv2->SetLineWidth(0);
@@ -70,73 +98,50 @@ void ppRefAnalyzer(){
   pad1->SetLogx();
   pad1->SetLogy();
   
-  TH1D * ppSpecD = new TH1D("specDummy1","",3,0.4,450);
-  ppSpecD->GetXaxis()->SetRangeUser(0.4,390);
-  ppSpecD->GetYaxis()->SetTitle("#frac{1}{N_{evt}} E #frac{d^{3}N}{dp^{3}} (GeV^{-2})");
+  TH1D * ppSpecD = new TH1D("specDummy1","",3,0.4,130);
+  ppSpecD->GetXaxis()->SetRangeUser(0.4,130);
+  ppSpecD->GetYaxis()->SetTitle("E #frac{d^{3}#sigma}{dp^{3}} (mb/GeV^{2})");
   ppSpecD->GetYaxis()->SetTitleOffset(1.4);
   ppSpecD->GetYaxis()->SetTitleSize(0.045);
   ppSpecD->GetYaxis()->SetLabelSize(0.04);
   ppSpecD->GetYaxis()->CenterTitle();
   ppSpecD->GetYaxis()->SetLabelOffset(0.002);
-  ppSpecD->GetYaxis()->SetRangeUser(1.1e-17,1e4);
-  ppSpecD->GetXaxis()->SetRangeUser(0.4,390);
+  ppSpecD->GetYaxis()->SetRangeUser(1.1e-12,1e4);
+  ppSpecD->GetXaxis()->SetRangeUser(0.4,130);
   ppSpecD->Draw();
-  ppSpec->SetMarkerStyle(5);
-  ppSpec->Scale(1/70.0);//scaled by inelastic xsection of 70 mb
-  ppSpec->Draw("same");
-  
-  pbpbSpec[0]->GetXaxis()->SetRangeUser(0.7,390);
-  pbpbSpec[0]->SetMarkerStyle(24);
-  pbpbSpec[0]->Scale(10);
-  pbpbSpec[0]->Draw("same");
-  pbpbSpec[1]->SetMarkerColor(kBlue);
-  pbpbSpec[1]->SetLineColor(kBlue);
-  pbpbSpec[1]->SetMarkerStyle(25);
-  pbpbSpec[1]->Scale(3);
-  pbpbSpec[1]->GetXaxis()->SetRangeUser(0.7,390);
-  pbpbSpec[1]->Draw("same");
-  pbpbSpec[23]->SetMarkerColor(kRed);
-  pbpbSpec[23]->SetLineColor(kRed);
-  pbpbSpec[23]->SetMarkerStyle(28);
-  pbpbSpec[23]->GetXaxis()->SetRangeUser(0.7,390);
-  pbpbSpec[23]->Draw("same");
-  pbpbSpec[24]->SetMarkerStyle(20);
-  pbpbSpec[24]->GetXaxis()->SetRangeUser(0.7,390);
-  pbpbSpec[25]->SetMarkerColor(kBlue);
-  pbpbSpec[25]->SetLineColor(kBlue);
-  pbpbSpec[24]->Draw("same");
-  pbpbSpec[25]->SetMarkerStyle(21);
-  pbpbSpec[25]->GetXaxis()->SetRangeUser(0.7,390);
-  pbpbSpec[25]->Draw("same");
-  pbpbSpec[30]->SetMarkerColor(kRed);
-  pbpbSpec[30]->SetLineColor(kRed);
-  pbpbSpec[30]->SetMarkerStyle(34);
-  pbpbSpec[30]->GetXaxis()->SetRangeUser(0.7,390);
-  pbpbSpec[30]->Draw("same");
-  TLegend * specLeg = new TLegend(0.25,0.1,0.45,0.5);
-  //specLeg->SetFillStyle(0);
-  specLeg->AddEntry((TObject*)0,"|#eta|<1",""); 
-  specLeg->AddEntry(pbpbSpec[0],Form("0-5%s (x10)","%"),"p");  
-  specLeg->AddEntry(pbpbSpec[1],Form("5-10%s (x3)","%"),"p");  
-  specLeg->AddEntry(pbpbSpec[23],Form("10-30%s","%"),"p");  
-  specLeg->AddEntry(pbpbSpec[24],Form("30-50%s","%"),"p");  
-  specLeg->AddEntry(pbpbSpec[25],Form("50-70%s","%"),"p");  
-  specLeg->AddEntry(pbpbSpec[30],Form("70-90%s","%"),"p");  
-  specLeg->AddEntry(ppSpec,"pp","p"); 
-  specLeg->Draw("same"); 
+  pp5->SetMarkerStyle(5);
+  pp5->Draw("same p");
+  pythia8_5->Scale(3);
+  pythia8_5->SetMarkerStyle(4);
+  pythia8_5->SetMarkerColor(kRed);
+  pythia8_5->SetLineColor(kRed);
+  pythia8_5->Draw("same p");
+  EPOS5->Scale(10);
+  EPOS5->SetMarkerStyle(25);
+  EPOS5->SetMarkerColor(kBlue);
+  EPOS5->SetLineColor(kBlue);
+  EPOS5->Draw("same p"); 
  
+  TLegend * specLeg = new TLegend(0.25,0.1,0.45,0.5);
+  specLeg->AddEntry((TObject*)0,"|#eta|<1",""); 
+  specLeg->AddEntry(pp5,"pp 5 TeV Data","p"); 
+  specLeg->AddEntry(pythia8_5,"Pythia 8 (x3)","p"); 
+  specLeg->AddEntry(EPOS5,"EPOS LHC (x10)","p"); 
+  specLeg->Draw("same"); 
+
   pad2->cd();
   pad2->SetLogx();
-  TH1D * ppSpecD2 = new TH1D("specDummy2","",3,0.4,450);
-  ppSpecD2->GetYaxis()->SetRangeUser(0.0,19.99);
+  TH1D * ppSpecD2 = new TH1D("specDummy2","",3,0.4,130);
+  ppSpecD2->GetYaxis()->SetRangeUser(0,2.0);
   ppSpecD2->GetYaxis()->SetNdivisions(4,4,0,kTRUE);
   ppSpecD2->GetYaxis()->SetTitleOffset(0.6);
   ppSpecD2->GetYaxis()->SetTitleFont(42);
   ppSpecD2->GetYaxis()->SetTitleSize(0.095);
   ppSpecD2->GetYaxis()->SetLabelSize(0.095);
   ppSpecD2->GetXaxis()->SetTitleFont(42);
-  ppSpecD2->GetYaxis()->SetTitle(Form("Syst. uncert. (%s)","%"));
-  ppSpecD2->GetXaxis()->SetRangeUser(0.4,390);
+  ppSpecD2->GetYaxis()->SetTitle(Form("MC/Data"));
+  ppSpecD2->GetYaxis()->CenterTitle();
+  ppSpecD2->GetXaxis()->SetRangeUser(0.4,130);
   ppSpecD2->GetXaxis()->SetTitle("p_{T} (GeV)");
   ppSpecD2->GetXaxis()->SetTitleSize(0.1);
   ppSpecD2->GetXaxis()->SetLabelSize(0.1);
@@ -144,45 +149,48 @@ void ppRefAnalyzer(){
   ppSpecD2->GetXaxis()->CenterTitle();
   ppSpecD2->GetXaxis()->SetTickLength(0.06);
   ppSpecD2->Draw();
-  s.PbPb_totSyst[0]->SetFillColor(kOrange);
-  s.PbPb_totSyst[0]->SetBinContent(1,0);
-  s.PbPb_totSyst[0]->SetBinError(1,0);
-  s.PbPb_totSyst[0]->SetBinContent(2,0);
-  s.PbPb_totSyst[0]->SetBinError(2,0);
-  s.PbPb_totSyst[0]->Scale(100);
-  s.PbPb_totSyst[0]->GetXaxis()->SetRangeUser(0.7,400);
-  s.PbPb_totSyst[0]->Draw("same");
-  s.PbPb_totSyst[30]->SetFillColor(kRed);
-  s.PbPb_totSyst[30]->SetFillStyle(3004);
-  s.PbPb_totSyst[30]->Scale(100);
-  s.PbPb_totSyst[30]->SetBinContent(1,0);
-  s.PbPb_totSyst[30]->SetBinError(1,0);
-  s.PbPb_totSyst[30]->SetBinContent(2,0);
-  s.PbPb_totSyst[30]->SetBinError(2,0);
-  s.PbPb_totSyst[30]->GetXaxis()->SetRangeUser(0.7,400);
-  s.PbPb_totSyst[30]->Draw("same");
-  s.pp_totSyst->SetFillColor(kBlack);
-  s.pp_totSyst->SetFillStyle(3003);
-  s.pp_totSyst->GetXaxis()->SetRangeUser(0.5,400);
-  s.pp_totSyst->Scale(100);
-  s.pp_totSyst->Draw("same");
-  TLegend * systLeg = new TLegend(0.6,0.6,0.9,0.98);
+  pp5relSyst_plus1->SetFillColor(kGray);
+  pp5relSyst_plus1->SetLineWidth(0);
+  //pp5relSyst->SetFillStyle(3003);
+  pp5relSyst_plus1->GetXaxis()->SetRangeUser(0.4,130);
+  pp5relSyst_minus1->SetFillColor(10);
+  pp5relSyst_minus1->SetFillStyle(1001);
+  pp5relSyst_minus1->SetLineWidth(0);
+  pp5relSyst_minus1->GetXaxis()->SetRangeUser(0.4,130);
+  pp5relSyst_plus1->Draw("same hist ][");
+  pp5relSyst_minus1->Draw("same hist ][");
+  pythia8_5rat->SetLineColor(kRed);
+  pythia8_5rat->SetMarkerSize(0);
+  EPOS5rat->SetLineColor(kBlue);
+  EPOS5rat->SetLineStyle(2);
+  EPOS5rat->SetMarkerSize(0);
+
+
+  TLegend * systLeg = new TLegend(0.6,0.75,0.9,0.98);
   //systLeg->SetFillStyle(0);
-  systLeg->AddEntry(s.PbPb_totSyst[0],Form("0-5%s","%"),"f");
-  systLeg->AddEntry(s.PbPb_totSyst[30],Form("70-90%s","%"),"f");
-  systLeg->AddEntry(s.pp_totSyst,"pp","f");
-  gStyle->SetPadTickY(1);
+  systLeg->AddEntry(pp5relSyst_plus1,"Data Uncertainty","f");
   systLeg->Draw("same");
   ppSpecD2->Draw("sameaxis");
   ppSpecD2->GetXaxis()->Draw("same");
+  pythia8_5rat->Draw("hist same c");
+  EPOS5rat->Draw("hist same c");
 
-  CMS_lumi( canv2, 0,33);
-  //canv2->Update();
-  //canv2->RedrawAxis();
-  //canv2->GetFrame()->Draw();    
-  canv2->SaveAs("plots/prettyPlots/Spectra_perEventYield.png");
-  canv2->SaveAs("plots/prettyPlots/Spectra_perEventYield.pdf");
-  canv2->SaveAs("plots/prettyPlots/Spectra_perEventYield.C");
+  TLine * line3 = new TLine(0.4,1,130,1);
+  line3->SetLineWidth(1);
+  line3->SetLineStyle(3);
+  line3->Draw("same");
+  
+  /*
+  lumi_sqrtS = "27.4 pb^{-1} (5.02 TeV pp)";
+  writeExtraText = true;  
+  extraText  = "Preliminary";
+  //extraText  = "Unpublished";
+  CMS_lumi(pad1, 0,33);
+  canv2->Update();
   */
+
+  canv2->SaveAs("img/Spectra_perEventYield.png");
+  canv2->SaveAs("img/Spectra_perEventYield.pdf");
+  canv2->SaveAs("img/Spectra_perEventYield.C");
 
 }
