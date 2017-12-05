b04c3be752b97f10270ad8359e94ca38963cf413
Austin Baty <abaty@submit.mit.edu>
2017-12-05 14:45:15 -0500

    included 7 teV

--- ppRef/EPOSMCFiles/pythia8MCAnalyzer.C
+++ ppRef/EPOSMCFiles/pythia8MCAnalyzer.C
@@ -8,15 +8,20 @@ void pythia8MCAnalyzer(){
   TH1::SetDefaultSumw2();
 
 
-  TFile * input = TFile::Open("EPOS5TeV_Nov16.root","read");
+  TFile * input = TFile::Open("EPOS5TeV_Dec2.root","read");
   TH1D * weightSumMB = (TH1D*)input->Get("weightSumMB");
   TH1D * pythia8MB = (TH1D*)input->Get("pythia8MB");
   pythia8MB->Scale(1.0/(float)weightSumMB->GetBinContent(1));
   
-  TFile * input544 = TFile::Open("EPOS544TeV_Nov16.root","read");
+  TFile * input544 = TFile::Open("EPOS544TeV_Dec2.root","read");
   TH1D * weightSumMB544 = (TH1D*)input544->Get("weightSumMB");
   TH1D * pythia8MB544 = (TH1D*)input544->Get("pythia8MB");
   pythia8MB544->Scale(1.0/(float)weightSumMB544->GetBinContent(1));
+  
+  TFile * input7 = TFile::Open("EPOS7TeV_Dec2.root","read");
+  TH1D * weightSumMB7 = (TH1D*)input7->Get("weightSumMB");
+  TH1D * pythia8MB7 = (TH1D*)input7->Get("pythia8MB");
+  pythia8MB7->Scale(1.0/(float)weightSumMB7->GetBinContent(1));
   
   TFile * output = TFile::Open("EPOSSpectra.root","recreate");
   output->cd();
@@ -25,6 +30,8 @@ void pythia8MCAnalyzer(){
   
   TH1D * pythia8_544 = (TH1D*)pythia8MB544->Clone("EPOS_544");
   //pythia8_544->Add(pythia8Hard544);
+  
+  TH1D * pythia8_7 = (TH1D*)pythia8MB7->Clone("EPOS_7");
 
   for(int i = 1; i<pythia8->GetSize()-1; i++){
     pythia8->SetBinContent(i,pythia8->GetBinContent(i)/(2.0*2.0*TMath::Pi()*(pythia8->GetBinLowEdge(i+1)-pythia8->GetBinLowEdge(i))*pythia8->GetBinCenter(i)));
@@ -32,6 +39,9 @@ void pythia8MCAnalyzer(){
     
     pythia8_544->SetBinContent(i,pythia8_544->GetBinContent(i)/(2.0*2.0*TMath::Pi()*(pythia8_544->GetBinLowEdge(i+1)-pythia8_544->GetBinLowEdge(i))*pythia8_544->GetBinCenter(i)));
     pythia8_544->SetBinError(i,pythia8_544->GetBinError(i)/(2.0*2.0*TMath::Pi()*(pythia8_544->GetBinLowEdge(i+1)-pythia8_544->GetBinLowEdge(i))*pythia8_544->GetBinCenter(i)));
+    
+    pythia8_7->SetBinContent(i,pythia8_7->GetBinContent(i)/(2.0*2.0*TMath::Pi()*(pythia8_7->GetBinLowEdge(i+1)-pythia8_7->GetBinLowEdge(i))*pythia8_7->GetBinCenter(i)));
+    pythia8_7->SetBinError(i,pythia8_7->GetBinError(i)/(2.0*2.0*TMath::Pi()*(pythia8_7->GetBinLowEdge(i+1)-pythia8_7->GetBinLowEdge(i))*pythia8_7->GetBinCenter(i)));
   }
 
   output->Write();
