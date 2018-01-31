2cf93cfe3c579130675969a414dd3434de461f41
Austin Baty <abaty@submit.mit.edu>
2018-01-31 16:37:18 -0500

    working on track distributions

--- plotting/makeTrackingPlots.C
+++ plotting/makeTrackingPlots.C
@@ -8,34 +8,54 @@
 #include "TPad.h"
 #include <string>
 
-void makeVzCent(TH1D * vz, TH1D * hiBin, TCanvas * c1){
+void makeVzCent(TH1D * vz, TH1D * hiBin,TH1D * MCvz, TH1D * MChiBin, TCanvas * c1){
+  c1->SetLeftMargin(0.12);
   vz->GetXaxis()->SetTitle("v_{z}");
-  vz->GetYaxis()->SetTitle("N_{evts}");
+  vz->GetYaxis()->SetTitle("normalized to unity");
+  vz->Scale(1.0/vz->GetEntries());
   vz->SetMarkerColor(kBlack);
   vz->SetLineColor(kBlack);
   vz->SetMarkerStyle(8);
+  vz->GetYaxis()->SetTitleOffset(1.2);
   vz->Draw("");
+  MCvz->Scale(1.0/MCvz->GetEntries());
+  MCvz->SetMarkerColor(kRed);
+  MCvz->SetLineColor(kRed);
+  MCvz->SetMarkerStyle(25);
+  MCvz->Draw("same");
 
   TLegend * l = new TLegend(0.15,0.65,0.45,0.85);
   l->AddEntry((TObject*)0,"Event Selections Applied","");
+  l->AddEntry(vz,"Data","p");
+  l->AddEntry(MCvz,"MB Hydjet","p");
   l->Draw("same");
   c1->SaveAs("trkPlots/vz.png");
   c1->SaveAs("trkPlots/vz.pdf");
   c1->SaveAs("trkPlots/vz.C");
 
   hiBin->GetXaxis()->SetTitle("hiBin");
-  hiBin->GetYaxis()->SetTitle("N_{evts}");
+  hiBin->GetYaxis()->SetTitle("normalized to unity");
+  hiBin->Rebin(4);
+  hiBin->Scale(1.0/hiBin->GetEntries());
   hiBin->SetMarkerColor(kBlack);
   hiBin->SetLineColor(kBlack);
   hiBin->SetMarkerStyle(8);
+  hiBin->GetYaxis()->SetTitleOffset(1.2);
   hiBin->Draw("");
+  MChiBin->Rebin(4);
+  MChiBin->Scale(1.0/MChiBin->GetEntries());
+  MChiBin->SetMarkerColor(kRed);
+  MChiBin->SetLineColor(kRed);
+  MChiBin->SetMarkerStyle(25);
+  MChiBin->Draw("same");
+  l->SetY1NDC(0.12);
+  l->SetY2NDC(0.4);
   l->Draw("same");
   hiBin->Draw("same");
 
   c1->SaveAs("trkPlots/hiBin.png");
   c1->SaveAs("trkPlots/hiBin.pdf");
   c1->SaveAs("trkPlots/hiBin.C");
-  
   delete l;
 }
 
@@ -50,12 +70,17 @@ void makeTrkDistInclusive(TH1D * h, TCanvas * c1, std::string Xlabel, std::strin
   h->SetMarkerColor(kBlack);
   h->SetLineColor(kBlack);
   h->SetMarkerStyle(8);
+  h->SetMarkerSize(0.5);
   h->GetYaxis()->SetTitleOffset(2);
-  h->Draw("h");
+  h->Draw("");
 
   TLegend * l = new TLegend(0.65,0.65,0.83,0.775);
   l->AddEntry((TObject*)0,"highPurity Tracks","");
-  l->AddEntry((TObject*)0,"|#eta|<1","");
+  if(strcmp(fileLabel.c_str(),"eta")!=0) l->AddEntry((TObject*)0,"|#eta|<1","");
+  l->AddEntry(h,"Data","p");
+  //leg->AddEntry((TObject*)0,"highPurity Tracks","");
+  //leg->AddEntry((TObject*)0,"highPurity Tracks","");
+  //leg->AddEntry((TObject*)0,"highPurity Tracks","");
   l->Draw("same");
   c1->SaveAs(Form("trkPlots/%s_Inclusive.png",fileLabel.c_str()));
   c1->SaveAs(Form("trkPlots/%s_Inclusive.pdf",fileLabel.c_str()));
@@ -97,11 +122,10 @@ int getHighCent(int i){
 void makeTrkDistArray(TH1D ** h, TCanvas * c2, std::string Xlabel, std::string fileLabel, bool doLogy = 0, float yMin = 0, float yMax = 1){
   TLegend * l[16]; 
   for(int i = 1; i<17; i++){
-    c2->cd(i);
-    l[i-1] = new TLegend(0.35,0.65,0.85,0.85);
-    l[i-1]->AddEntry((TObject*)0,"highPurity Tracks","");
-    l[i-1]->AddEntry((TObject*)0,Form("%1.1f<p_{T}<%1.1f, |#eta|<1",getLowPt(i),getHighPt(i)),"");
-    l[i-1]->AddEntry((TObject*)0,Form("%d%%-%d%%",getLowCent(i),getHighCent(i)),"");
+    c2->cd(i+4);
+    l[i-1] = new TLegend(0.55,0.65,0.875,0.85);
+    l[i-1]->AddEntry((TObject*)0,Form("%1.1f<p_{T}<%1.0f",getLowPt(i),getHighPt(i)),"");
+    l[i-1]->AddEntry((TObject*)0,Form("%d-%d%%",getLowCent(i),getHighCent(i)),"");
     if(doLogy==1) c2->SetLogy();
     h[i]->SetTitle("");
     h[i]->Scale(1.0/h[i]->GetEntries());
@@ -109,13 +133,27 @@ void makeTrkDistArray(TH1D ** h, TCanvas * c2, std::string Xlabel, std::string f
     h[i]->GetYaxis()->SetRangeUser(yMin,yMax);
     h[i]->GetYaxis()->SetTitle("Normalized to unity");
     h[i]->SetMarkerColor(kBlack);
+    h[i]->SetMarkerStyle(8);
+    h[i]->SetMarkerSize(0.5);
     h[i]->SetLineColor(kBlack);
     //h[i]->SetMarkerStyle(8);
     h[i]->GetYaxis()->SetTitleOffset(2);
-    h[i]->Draw("h"); 
+    h[i]->Draw(""); 
 
     l[i-1]->Draw("same");
+    h[i]->Draw("same"); 
   }
+
+  c2->cd(1);
+  TLegend * leg = new TLegend(0,0,1,1);
+  leg->AddEntry((TObject*)0,"Plots Normalized to Unity","");
+  leg->AddEntry((TObject*)0,"highPurity Tracks","");
+  if(strcmp(fileLabel.c_str(),"eta")!=0) leg->AddEntry((TObject*)0,"|#eta|<1","");
+  leg->AddEntry(h[1],"Data","p");
+  //leg->AddEntry((TObject*)0,"highPurity Tracks","");
+  //leg->AddEntry((TObject*)0,"highPurity Tracks","");
+  //leg->AddEntry((TObject*)0,"highPurity Tracks","");
+  leg->Draw("same");
   c2->SaveAs(Form("trkPlots/%s_array.png",fileLabel.c_str()));
   c2->SaveAs(Form("trkPlots/%s_array.pdf",fileLabel.c_str()));
   c2->SaveAs(Form("trkPlots/%s_array.C",fileLabel.c_str()));
@@ -129,8 +167,10 @@ void makeTrackingPlots(){
   gStyle->SetOptStat(0);
   
   TFile * f = TFile::Open("../output_0.root","read");
-  
+  TFile * mc = TFile::Open("MCTrackingRootFiles/Hydjet_Jan31.root","read"); 
+ 
   TH1D *nHit[17], *chi2[17], *DCAz[17], *DCAxy[17], *ptErr[17], *eta[17], *phi[17], *caloMatch[17];
+  TH1D *MCnHit[17][3], *MCchi2[17][3], *MCDCAz[17][3], *MCDCAxy[17][3], *MCptErr[17][3], *MCeta[17][3], *MCphi[17][3], *MCcaloMatch[17][3];
   for(int c = 0; c<17; c++){
     nHit[c] = (TH1D*)f->Get(Form("nHit%d",c));
     chi2[c] = (TH1D*)f->Get(Form("chi2%d",c));
@@ -140,12 +180,24 @@ void makeTrackingPlots(){
     eta[c] = (TH1D*)f->Get(Form("eta%d",c));
     phi[c] = (TH1D*)f->Get(Form("phi%d",c));
     caloMatch[c] = (TH1D*)f->Get(Form("caloMatch%d",c));
+    for(int c2 = 0; c2<3; c2++){
+      MCnHit[c][c2] = (TH1D*)mc->Get(Form("nHit%d_%d",c,c2));
+      MCchi2[c][c2] = (TH1D*)mc->Get(Form("chi2%d_%d",c,c2));
+      MCptErr[c][c2] = (TH1D*)mc->Get(Form("ptErr%d_%d",c,c2));
+      MCDCAz[c][c2] = (TH1D*)mc->Get(Form("DCAz%d_%d",c,c2));
+      MCDCAxy[c][c2] = (TH1D*)mc->Get(Form("DCAxy%d_%d",c,c2));
+      MCeta[c][c2] = (TH1D*)mc->Get(Form("eta%d_%d",c,c2));
+      MCphi[c][c2] = (TH1D*)mc->Get(Form("phi%d_%d",c,c2));
+      MCcaloMatch[c][c2] = (TH1D*)mc->Get(Form("caloMatch%d_%d",c,c2));
+    }
   }
   TH1D * vz = (TH1D*)f->Get("vz");
   TH1D * hiBin = (TH1D*)f->Get("hiBin");
+  TH1D * MCvz = (TH1D*)mc->Get("vz");
+  TH1D * MChiBin = (TH1D*)mc->Get("hiBin");
 
   TCanvas * c1 = new TCanvas("c1","c1",800,600);
-  makeVzCent(vz,hiBin,c1);
+  makeVzCent(vz,hiBin,MCvz,MChiBin,c1);
 
   makeTrkDistInclusive(nHit[0],c1,"nHits","nHits",0,0,0.12);
   makeTrkDistInclusive(chi2[0],c1,"#chi^{2}/ndof/nLayers","chi2",0,0,0.075);
@@ -158,8 +210,8 @@ void makeTrackingPlots(){
   delete c1;
 
   //split by cent and pt
-  TCanvas * c2 = new TCanvas("c2","c2",1000,1000);
-  c2->Divide(4,4); 
+  TCanvas * c2 = new TCanvas("c2","c2",1000,1250);
+  c2->Divide(4,5); 
   makeTrkDistArray(nHit,c2,"nHits","nHits",0,0,0.25);
   makeTrkDistArray(chi2,c2,"#chi^{2}/ndof/nLayers","chi2",0,0,0.075);
   makeTrkDistArray(DCAz,c2,"d_{z}/#sigma_{z}","DCAz",0,0,0.1);     
