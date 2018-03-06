4516a0fa17d0ad203dbbbab1f5602128dccdd7e5
Austin Baty <abaty@submit.mit.edu>
2018-03-05 19:45:50 -0500

    added some cancellation stuff for RAA

--- systematics.C
+++ systematics.C
@@ -3,6 +3,7 @@
 #include "TH1D.h"
 #include "TH2D.h"
 #include "include/trackingCorrection.h"
+#include "include/trackingDataMCDiffUncert.h"
 
 void systematics(){
   Settings s = Settings();
@@ -32,6 +33,15 @@ void systematics(){
   TH1D * spec_FakeCorr[s.nCentBins]; 
   TH1D * spec_EffMethod[s.nCentBins]; 
   TH1D * spec_Total[s.nCentBins]; 
+
+  TH1D * RAA_reso[s.nCentBins];
+  TH1D * RAA_MCvsDataEff[s.nCentBins]; 
+  TH1D * RAA_SpecCorr[s.nCentBins];
+  TH1D * RAA_FakeCorr[s.nCentBins];  
+  TH1D * RAA_EffMethod[s.nCentBins];  
+  TH1D * RAA_ppRef[s.nCentBins];
+  TH1D * RAA_Total[s.nCentBins]; 
+
   for(int i = 0; i<s.nCentBins; i++){
     spec_reso[i] = new TH1D(Form("spec_reso_%d",i),"",s.ntrkBins,s.xtrkbins);
     spec_MCvsDataEff[i] = new TH1D(Form("spec_MCvsDataEff_%d",i),"",s.ntrkBins,s.xtrkbins);
@@ -39,6 +49,14 @@ void systematics(){
     spec_FakeCorr[i] = new TH1D(Form("spec_fakeCorr_%d",i),"",s.ntrkBins,s.xtrkbins);
     spec_EffMethod[i] = new TH1D(Form("spec_effMethod_%d",i),"",s.ntrkBins,s.xtrkbins);
     spec_Total[i] = new TH1D(Form("spec_Total_%d",i),"",s.ntrkBins,s.xtrkbins);
+ 
+    RAA_reso[i]  = new TH1D(Form("RAA_reso_%d",i),"",s.ntrkBins,s.xtrkbins);
+    RAA_MCvsDataEff[i] = new TH1D(Form("RAA_MCvsDataEff_%d",i),"",s.ntrkBins,s.xtrkbins); 
+    RAA_SpecCorr[i] = new TH1D(Form("RAA_SpecCorr_%d",i),"",s.ntrkBins,s.xtrkbins); 
+    RAA_FakeCorr[i]  = new TH1D(Form("RAA_FakeCorr_%d",i),"",s.ntrkBins,s.xtrkbins);
+    RAA_EffMethod[i]  = new TH1D(Form("RAA_EffMethod_%d",i),"",s.ntrkBins,s.xtrkbins);
+    RAA_ppRef[i] = new TH1D(Form("RAA_ppRef_%d",i),"",s.ntrkBins,s.xtrkbins);
+    RAA_Total[i] = new TH1D(Form("RAA_Total_%d",i),"",s.ntrkBins,s.xtrkbins);
   }
    
 
@@ -55,7 +73,7 @@ void systematics(){
       total2 += TMath::Power(s.HI_UpFakeCorr[i]->GetBinContent(j)-1,2);
       
       //efficiency uncertainty from MC stats
-      spec_EffMethod[i]->SetBinContent(j,0.03);
+      spec_EffMethod[i]->SetBinContent(j,0.04);
       total2 += TMath::Power(0.04,2);
 
       //resolution is a flat 0.5%
@@ -71,6 +89,38 @@ void systematics(){
     }
   }
 
-  output->Write(); 
+  //RAA
+  for(int i = 0; i<s.nCentBins; i++){
+    for(int j = 1; j<s.ntrkBins+1; j++){
+      double total2 = 0; 
+      //pp reference without lumi and with pp data/MC Eff difference removed
+      //note that the total2 thing is the square of a sqrt
+      RAA_ppRef[i]->SetBinContent(j,TMath::Sqrt(TMath::Power(ppSyst_NoLumi->GetBinContent(j),2)-0.04*0.04));
+      total2 += TMath::Power(ppSyst_NoLumi->GetBinContent(j),2)-0.04*0.04;
 
+      //species corr
+      RAA_SpecCorr[i]->SetBinContent(j,TMath::Abs(s.HI_UpSpecCorr[i]->GetBinContent(j)-1));
+      total2 += TMath::Power(s.HI_UpSpecCorr[i]->GetBinContent(j)-1,2);
+      
+      //fake correction
+      RAA_FakeCorr[i]->SetBinContent(j,TMath::Abs(s.HI_UpFakeCorr[i]->GetBinContent(j)-1));
+      total2 += TMath::Power(s.HI_UpFakeCorr[i]->GetBinContent(j)-1,2);
+      
+      //efficiency uncertainty from MC stats
+      RAA_EffMethod[i]->SetBinContent(j,0.04);
+      total2 += TMath::Power(0.04,2);
+
+      //resolution
+      RAA_reso[i]->SetBinContent(j,0.005);
+      total2 += TMath::Power(0.005,2);
+      
+      // MC vs data uncert assumed to cancle at least as well at PbPb 
+      RAA_MCvsDataEff[i]->SetBinContent(j,trackingDataMCDiffUncert(RAA_MCvsDataEff[i]->GetBinCenter(j),s.lowCentBin[i]+1));
+      total2 += TMath::Power(trackingDataMCDiffUncert(RAA_MCvsDataEff[i]->GetBinCenter(j),s.lowCentBin[i]+1),2);
+
+      //total
+      RAA_Total[i]->SetBinContent(j,TMath::Sqrt(total2));
+    }
+  }
+  output->Write(); 
 }
